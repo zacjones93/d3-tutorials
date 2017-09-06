3df60bb935ee36c4b3107d2e1e1b6760b18227af
Zac Jones <zacjones93@gmail.com>
2017-09-05 17:10:54 -0700

    convert Dates to linear values

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
-var linearScale = d3.scaleLinear()
-  .domain([0, 100]) // range of possible value of input
-  .range([0, 600]) // normalize output so they fall somewhere 0-1
-  .clamp(true) // clampped max/min output of scale
+var timeScale = d3.scaleTime()
+  .domain([new Date(2016, 0 ,1), new Date()])
+  .range([0, 100]);
 
-console.log(linearScale(0))
-console.log(linearScale(50))
-console.log(linearScale(105))
+  // takes an input range of January 1, 2016 - now and outputs it on a linear scale from 0 - 100
 
-console.log(linearScale.invert(300)) // range -> domain
+console.log(timeScale(new Date(2016, 4, 15)));
+console.log(timeScale(new Date(2016, 10, 3)));
+console.log(timeScale(new Date(2017, 4, 15)));
+
+console.log(timeScale.invert(50))
