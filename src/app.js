bac3ef0fe74ca7346c22c3ff26303712a802bfd3
Zac Jones <zacjones93@gmail.com>
2017-09-05 23:06:24 -0700

    starwars chart

--- src/app.js
+++ src/app.js
@@ -1,34 +1,3 @@
-var scores = [
-  { name: 'Alice', score: 96 },
-  { name: 'Billy', score: 83 },
-  { name: 'Cindy', score: 91 },
-  { name: 'David', score: 96 },
-  { name: 'Emily', score: 88 }
-];
-
-let update = d3.select('.chart')
-  .selectAll('div')
-  .data(scores, function (d) {
-    return d ? d.name : this.innerText;
-  })
-  .style('color', 'blue')
-
-  let enter = update.enter()
-    .append('div')
-    .text(function (d) {
-      return d.name;
-    })
-    .style('color', 'green');
-
-  update.exit().remove();
-
-  update.merge(enter)
-    .style('width', d => d.score + 'px')
-    .style('height', '50px')
-    .style('background', 'lightgreen')
-    .style('border', '1px solid black')
-// ============================================
-
 let data;
 // fetches data from api
 d3.json('https://swapi.co/api/people', (err, json) => {
