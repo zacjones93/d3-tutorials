4eeef5388f7a2a32b7f4e53e70e51638c0b14e4e
Zac Jones <zacjones93@gmail.com>
2017-09-10 20:34:10 -0700

    basic svg output and styling

--- src/app.js
+++ src/app.js
@@ -1,33 +1,26 @@
 let data;
 // fetches data from api
-d3.json('https://swapi.co/api/people', (err, json) => {
-  if (err) return console.warn(err);
-// selects divs in the .chart-2 class
-  let update = d3.select(".chart-2")
-    .selectAll('div')
-    .data(json.results, (d) => {
-      return d ? d.name : this.innerText;
-    })
-// adds divs based on the amount of items in fetched data    
-  let enter = update.enter()
-  .append('div')
-  .text((d) => {
-    return d.name
+var scores = [
+  { name: 'Alice', score: 96 },
+  { name: 'Billy', score: 83 },
+  { name: 'Cindy', score: 91 },
+  { name: 'David', score: 96 },
+  { name: 'Emily', score: 88 }
+];
+
+let update = d3.select('.chart')
+  .append('svg')
+    .attr('width', 225)
+    .attr('height', 300)
+  .selectAll('rect')
+  .data(scores, function (d) {
+    return d ? d.name : this.innerText;
   })
-  .style('color', 'black')
-// styles divs based on data attributes
-  update.merge(enter)
-    .style('width', d => d.mass + 'px')
-    .style('height', d => '50px')
-    .style('background', d => {
-      if(d.gender === 'male'){
-        return 'lightblue'
-      } else if(d.gender === 'female') {
-        return 'pink'
-      } else {
-        return 'gray'
-      }
-       
+  .enter()
+    .append('rect')
+    .attr('y', (d, i) => i * 33)
+    .style('width', d => d.score)
+    .text(function (d) {
+      return d.name;
     })
-    .style('border', '1px solid black')
- })
+    .attr('class', 'bar')
