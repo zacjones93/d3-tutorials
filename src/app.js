347250f1d2dd617210ee4600554169077df69533
Zac Jones <zacjones93@gmail.com>
2017-10-02 17:13:20 -0700

    margin convention

--- src/app.js
+++ src/app.js
@@ -1,58 +1,23 @@
-var scores = [
-  { name: 'Alice', score: 96 },
-  { name: 'Billy', score: 83 },
-  { name: 'Cindy', score: 91 },
-  { name: 'David', score: 96 },
-  { name: 'Emily', score: 88 }
-];
+var margin = { top: 10, right: 20, bottom: 25, left: 25 };
+var width = 425 - margin.left - margin.right;
+var height = 625 - margin.top - margin.bottom;
 
-var bar = d3.select('.chart')
-  .append('svg')
-    .attr('width', 225)
-    .attr('height', 300)
-  .selectAll('g')
-  .data(scores)
-  .enter()
-    .append('g')
-    .attr('transform', (d, i) => 'translate(0, ' + i * 33 + ')')
-    
+var svg = d3.select('.chart')
+.append('svg')
+  .attr('width', width + margin.left + margin.right)
+  .attr('height', height + margin.top + margin.bottom)
+.append('g')
+  .attr('transform', `translate(${margin.left}, ${margin.top})`);
 
-function scaleBar (selection, scale) {
-  selection.style('transform', 'scaleX(' + scale + ')')
-}
+svg.append('rect')
+  .attr('width', width / 2)
+  .attr('height', height)
+  .style('fill', 'lightblue')
+  .style('stroke', 'green');
 
-function fade (selection, opacity) {
-  selection.style('fill-opacity', opacity)
-}
-
-function setFill (selection, color) {
-  selection.style('fill', color)
-}
-
-
-bar.append('rect')
-    .style('width', d => d.score)
-    .attr('class', 'bar')
-    .on('mouseover', function (d, i, elements) {
-      d3.select(this)
-        .call(scaleBar, 2)
-        .call(setFill, 'orange')
-       
-      d3.selectAll(elements)
-        .filter(':not(:hover)')
-        .call(fade, 0.5)
-    })
-    .on('mouseout', function (d, i, elements) {
-      d3.select(this)
-        .call(scaleBar, 1)
-        .call(setFill, 'lightgreen')
-
-      d3.selectAll(elements)
-        .call(fade, 1)
-    });
-
-bar.append('text')
-  .attr('y', 20)
-  .text(function (d) {
-    return d.name;
-  });
+svg.append('rect')
+.attr('x', width / 2)
+  .attr('width', width / 2)
+  .attr('height', height)
+  .style('fill', 'lightblue')
+  .style('stroke', 'green');
